3f0352730eaa47c98b584fa24f79a4bedec1432f
Yanguk <yanguk@duck.com>
2025-09-27 07:35:14 +0900

    add: profile avatar font
    Signed-off-by: Yanguk <yanguk@duck.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,6 +13,11 @@ export default function Home() {
           height={38}
           priority
         />
+
+        <Avatar>
+          <AvatarFallback className="font-backyard">YU</AvatarFallback>
+        </Avatar>
+
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
